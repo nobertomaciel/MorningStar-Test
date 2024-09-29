22a99bb463680f0c7048c72c6647d36c605c0b2a
nobertomaciel <nobertomaciel@hotmail.com>
2024-09-28 23:33:32 -0300

    v1.02 form entrada e saída feito - sem conexão com BD

--- frontend/src/operacoes/entrada.js
+++ frontend/src/operacoes/entrada.js
@@ -1,22 +1,18 @@
 import React, { useState } from "react";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
 import Select from '../select'
 import { Dialog } from 'primereact/dialog';
-
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 function Entrada() {
-
     const [visible, setVisible] = useState(false);
     return (
     <div className="modal">
-    <button className="entrada" label="Show" icon="pi pi-external-link" onClick={() => setVisible(true)}>Entrada</button>
-    <Dialog header={"Registrar entrada"} visible={visible} style={{ width: '50vw', height: '50vw'}} onHide={() => {if (!visible) return; setVisible(false); }}>
-        <Select/>
-        <p className="m-0">
-            
-        
-        </p>
-    </Dialog>
+        <button className="entrada" label="Show" icon="pi pi-external-link" onClick={() => setVisible(true)}>Entrada</button>
+        <Dialog header={"Registrar entrada"} visible={visible} style={{ width: '50vw', height: '50vw'}} onHide={() => {if (!visible) return; setVisible(false); }}>
+            <Select/>
+            <button className="close" label="Show" icon="pi pi-external-link" onClick={() => setVisible(false)}>Fechar</button>
+            <button className="save" label="Show" icon="pi pi-external-link" onClick={() => setVisible(true)}>Salvar</button>
+        </Dialog>
     </div>)
 }
 
--- frontend/src/select.js
+++ frontend/src/select.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
+import { InputNumber } from 'primereact/inputnumber';
+        
+
 
 function productSelect(){
     const apiUrl = "http://localhost:5000/data";
     const [selectedOption, setSelectedOption] = useState(null);
     const options = [];
-    // const options = [
-    //   { value: 'chocolate', label: 'Chocolate' }
-    // ];
-
+    const [quant, setQuant] = useState(1);
     async function getData() {
       const result = await fetch(apiUrl, {
         method: "GET",
@@ -17,23 +17,24 @@ function productSelect(){
           "Access-Control-Allow-Origin": "*"
         }
       });
-      console.log(result);
       const getResult = await result.json();
-      console.log(getResult);
-
-      getResult.forEach(e => {        
-        options.push({value: e.idProduct, label: e.productName});
+      getResult.forEach(e => {
+        options.push({value: e.id, label: e.productName});
       });
     }
+
     getData();
 
     return (
-      <div>
-        <Select
-          defaultValue={selectedOption}
-          onChange={setSelectedOption}
-          options={options}
-        />
+      <div className="card flex flex-wrap gap-3 p-fluid">
+        <div className="flex-auto">
+        <label htmlFor="produto" className="font-bold block mb-2">Produto</label>
+        <Select inputId="produto" defaultValue={selectedOption} onChange={setSelectedOption} options={options} />
+        </div>
+        <div className="flex-auto">
+                <label htmlFor="quantidade" className="font-bold block mb-2">Quantidade</label>
+                <InputNumber inputId="quantidade" value={quant} onValueChange={(e) => setQuant(e.value)} mode="decimal" showButtons min={1} max={10000000} />
+        </div>
       </div>
     );
 }
